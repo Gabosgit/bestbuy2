5b3bc88e31e9f585f02f4077d63975579522586e
Gabo <no@email>
2025-01-23 14:26:09 +0100

    Working/testing on test_produnct.py

--- test_product.py
+++ test_product.py
@@ -1,13 +1,28 @@
-"""
-"""
-
 import pytest
+import products
+
+#Create a normal product
+def test_create_prod():
+    products.Product("my_prod", price=100, quantity=100)
+
+# Empty name
+def test_empty_name():
+    with pytest.raises(Exception):
+        products.Product("", price=1450, quantity=100)
+
+def test_negative_price():
+    with pytest.raises(Exception):
+        products.Product("MacBook Air M2", price=-10, quantity=100)
+
+def test_prod_inactive():
+    my_prod = products.Product("MacBook Air M2", price=10, quantity=10)
+    my_prod.set_quantity(0)
+    if my_prod.is_active() == False:
+        return True
 
 
 
-def main():
-    pass
+# test_empty_name()
+# test_negative_price()
 
 
-if __name__ == '__main__':
-    main()

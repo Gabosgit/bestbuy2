81cc833e664d2934ee594ab079065c3e55bce389
Gabo <no@email>
2025-01-29 14:00:37 +0100

    test_product.py was updated

--- test_product.py
+++ test_product.py
@@ -1,7 +1,7 @@
 import pytest
 import products
 
-#Create a normal product
+# Create a normal product
 def test_create_prod():
     products.Product("my_prod", price=100, quantity=100)
 
@@ -10,19 +10,25 @@ def test_empty_name():
     with pytest.raises(Exception):
         products.Product("", price=1450, quantity=100)
 
+# Negative Price()
 def test_negative_price():
     with pytest.raises(Exception):
         products.Product("MacBook Air M2", price=-10, quantity=100)
 
+# Product is inactive
 def test_prod_inactive():
     my_prod = products.Product("MacBook Air M2", price=10, quantity=10)
     my_prod.set_quantity(0)
-    if my_prod.is_active() == False:
-        return True
+    assert my_prod.is_active() == False
 
+# Test that product purchase modifies the quantity and returns the right output.
+def test_update_quantity():
+    my_prod = products.Product("MacBook Air M2", price=10, quantity=10)
+    my_prod.buy(5)
+    assert my_prod.get_quantity() == 5
 
-
-# test_empty_name()
-# test_negative_price()
-
-
+# Test that buying a larger quantity than exists invokes exception.
+def test_buying_larger_quantity():
+    with pytest.raises(Exception):
+        my_prod = products.Product("MacBook Air M2", price=10, quantity=10)
+        my_prod.buy(11)
